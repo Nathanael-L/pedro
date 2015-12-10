ebf4bdee0066830b7fb52f7554efe7dab06dfbcf
Nathanael Lang <nathanael.lang@gmail.com>
2015-12-10 13:05:10 +0100

    constrast.hpp

---
+++ src/contrast.hpp
@@ -0,0 +1,15 @@
+/***
+ * contrast.hpp
+ *
+ *  Created on: Dec 10, 2015
+ *      Author: nathanael
+ */
+
+#ifndef CONTRAST_HPP_
+#define CONTRAST_HPP_
+
+class Constrast {
+
+};
+
+#endif /* CONTRAST_HPP_ */
