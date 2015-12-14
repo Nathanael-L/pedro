19a1a15017268fe13d73f941daa3e46f02e04885
Nathanael Lang <nathanael.lang@gmail.com>
2015-12-14 17:31:19 +0100

    work on detecting only the closer sidewalk. outworn idea over connection strings.

--- src/contrast.hpp
+++ src/contrast.hpp
@@ -28,7 +28,6 @@ class Contrast {
         vector<Coordinate> splits;
         LineSegment segment(start, end);
         double length = go.haversine(start.x, start.y, end.x, end.y);
-        cout << "length" << length << endl;
         double fraction = fraction_length / length;
         double position = 0;
 
@@ -42,14 +41,12 @@ class Contrast {
     }
 
     double compare_to_length(int count_intersects, double length) {
-        cout << length << " / " << segment_size << endl;
         double count_segments = length / segment_size;
         /*int count_segments = static_cast<int>(floor(length / segment_size));
         if (count_segments == 0) {
             count_segments = 1;
         }*/
         double ratio = static_cast<double>(count_intersects) / count_segments;
-        cout << "ratio: " << ratio << endl;
         return ratio;
     }
 
@@ -77,13 +74,57 @@ public:
         }
     }
 
+    /***
+     *
+     * Look up for neighbour sidewalk in ds.finished_connections, if there is,
+     * it compares the distance to the mid of the orthogonal.
+     * Return true if input is the closer sidewalk or if no neighbour exists.
+     * BUG: if just one sidewalk is on the farer side.
+     *
+     */
+    bool closer_sidewalk(Sidewalk* sidewalk, Geometry* ortholine) {
+
+        String connection_string = sidewalk->connection_string;
+        auto sidewalk_pair = ds.finished_segments.find(conncection_string)->second;
+        if (sidewalk_pair != ds.finished_segments.end()) {
+            cerr << "Unexpected error: connection_string doesn't show up"
+                    << " in map." << endl
+                    << "In check_distance / contrast.hpp" << endl;
+            exit(1);
+        }
+        Sidewalk* neighbour_sidewalk = nullptr;
+        if ((sidewalk_pair->first) && (sidewalk == sidewalk_pair->first)) {
+            neighbour_sidewalk = sidewalk_pair->second;
+        } else {
+            neighbour_sidewalk = sidewalk_pair->first;
+        } // neighbour can be either null or pointer on Sidewalk-Object
+        if (!neighbour_sidewalk) {
+            return false;
+        } // maybe true is right here. musst be tested
+        Point* ortho_mid = ortholine->getCentroid();
+        double ortho_lon = ortho_mid->getX();
+        Geometry* sidewalk_intersection = ortho.intersection(
+                sidewalk->geometry);
+        Geometry* neighbour_intersection = ortho.intersection(
+                neighbour_sidewalk->geometry);
+        double sidewalk_lon = dynamic_cast<Point*>(sidewalk_intersection)->getX();
+        double neighbour_lon = dynamic_cast<Point*>(neighbour_intersection)->getX();
+        double distance_sidewalk = abs(sidewalk_lon - ortho_lon);
+        double distance_neighbour = abs(sidewalk_lon - ortho_lon);
+        if (distace_sidewalk > distance_neighbour) {
+            return false;
+        }
+        return true;
+    }
+
     void check_sidewalks() {
-        cout << "count sidewalks: " << ds.sidewalk_set.size() << endl;
+        google::sparse_hash_map<string, vector<Sidewalk*>> detect_map;
+        detect_map.set_delete_key("");
         for (auto sidewalk_road : ds.sidewalk_set) {
             vector<void *> results;
             Geometry* sidewalk = sidewalk_road->geometry;
             ds.ortho_tree.query(sidewalk->getEnvelopeInternal(), results);
-            if (results.size() > 0) {
+            if (results.size() > 1) {
                 int count_intersects = 0;
                 for (auto result : results) {
                     Geometry* ortho_line = static_cast<Geometry*>(result);
@@ -95,12 +136,16 @@ public:
                 double ratio = compare_to_length(count_intersects,
                         sidewalk_road->length);
                 if (ratio > contrast_factor) {
-                    ds.insert_intersect(sidewalk, sidewalk_road->length, ratio);
+                    //ds.insert_intersect(sidewalk, sidewalk_road->length, ratio);
+                    string osm_id = sidewalk_road->osm_id;
+                    detect_map[osm_id].push_back(sidewalk_road);
+                    if (detect_map[osm_id].size > 1) {
+                        
+                    }
                 }
             }
         }
     }
-
 };
 
 #endif /* CONTRAST_HPP_ */
